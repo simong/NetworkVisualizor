17323bfdbde42d099eae80b42bd0f421dfab9a6b
Simon G <gaeremyncks@gmail.com>
2010-11-27 23:58:26 +0100

    Cleaned up the custom nodetypes.

--- js/jit.extensions.js
+++ js/jit.extensions.js
@@ -1,48 +1,31 @@
 (function(){
-  var img_pc_infected = new Image();
-  img_pc_infected.src = "images/icons/infected.png";
-  var img_pc_clean = new Image();
-  img_pc_clean.src = "images/icons/clean.png";
-  
-   $jit.ForceDirected.Plot.NodeTypes.implement({  
-   'infected': {  
-     'render': function(node, canvas) {  
-       //print your custom node to canvas
-       canvas.getCtx().drawImage(img_pc_infected, node.endPos.x-15, node.endPos.y-30);
-      //var ctx = canvas.getCtx();
-      //ctx.beginPath();
-      //ctx.arc(pos.x, pos.y, radius, 0, Math.PI * 2, true);
-      //ctx.closePath();
-      //ctx[type]();
-       
-     },  
-     //optional  
-     'contains': function(node, pos) {  
-       //return true if pos is inside the node or false otherwise
-       return false;
-     }  
-   }  
- }); 
-  
+    var img_pc_infected = new Image();
+    img_pc_infected.src = "images/icons/infected.png";
+    var img_pc_clean = new Image();
+    img_pc_clean.src = "images/icons/clean.png";
     
-   $jit.ForceDirected.Plot.NodeTypes.implement({  
-   'clean': {  
-     'render': function(node, canvas) {  
-       //print your custom node to canvas
-       canvas.getCtx().drawImage(img_pc_clean, node.endPos.x-15, node.endPos.y-30);
-      //var ctx = canvas.getCtx();
-      //ctx.beginPath();
-      //ctx.arc(pos.x, pos.y, radius, 0, Math.PI * 2, true);
-      //ctx.closePath();
-      //ctx[type]();
-       
-     },  
-     //optional  
-     'contains': function(node, pos) {  
-       //return true if pos is inside the node or false otherwise
-       return false;
-     }  
-   }  
- });
- 
- })();
+    $jit.ForceDirected.Plot.NodeTypes.implement({
+        'infected': {
+            'render': function(node, canvas){
+                canvas.getCtx().drawImage(img_pc_infected, node.endPos.x - 15, node.endPos.y - 30);
+            },
+            'contains': function(node, pos){
+                return ((pos.x >= node.endPos.x && pos.x <= node.endPos.x + 55) &&
+                (pos.y >= node.endPos.y && pos.y <= node.endPos.y + 80));
+            }
+        }
+    });
+    
+    $jit.ForceDirected.Plot.NodeTypes.implement({
+        'clean': {
+            'render': function(node, canvas){
+                canvas.getCtx().drawImage(img_pc_clean, node.endPos.x - 15, node.endPos.y - 30);
+            },
+            'contains': function(node, pos){
+                return ((pos.x >= node.endPos.x && pos.x <= node.endPos.x + 33) &&
+                (pos.y >= node.endPos.y && pos.y <= node.endPos.y + 61));
+            }
+        }
+    });
+    
+})();
